8e994c8f258ba61ab9a367d70e09498976676af0
Logan Rowe <rowe1227@gmail.com>
2019-07-16 01:30:17 -0500

    Added scripts for filtering images based on light level and identifying hypersensitive pixels

---
+++ 2 Ave and STD Light and Shear Values.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 28 15:52:01 2018
+
+@author: Logan Rowe
+Saves a comma delimited list of the wafer number, (1), (2), (3), (4)
+for each wafer in the designated savefile
+
+1) Average of Average Light Value
+2) Average of STD Light Value
+3) STD of Average Light Value
+4) STD of STD Light Value
+5) Average of Shear Max Before Subtraction Image
+
+These values will be used to help filter out poor quality images
+"""
+
+import numpy as np
+import os
+import glob
+
+
+datafile='C:\\Users\\Logan Rowe\\Desktop\\bowtie-defect-identification\\Light Levels\\Data'
+savefile='C:\\Users\\Logan Rowe\\Desktop\\bowtie-defect-identification\\Light Levels'
+os.chdir(datafile)
+
+
+
+filelist=[]
+for i in glob.glob('*d45.t*'):
+    filelist.append(i)
+
+waferlist=[i.split('_')[0] for i in filelist]
+AA=[] #SEE ITEMS (1-4) AT TOP
+AS=[]
+SA=[]
+SS=[]
+MM=[]
+
+
+for i in filelist:
+    aveL,stdL,dM,d0,d45=np.genfromtxt(i,delimiter=',',unpack=True)
+    AA.append(np.mean(aveL))
+    AS.append(np.mean(stdL))
+    SA.append(np.std(aveL))
+    SS.append(np.std(stdL))
+    MM.append(np.mean(dM))
+    
+#WRITE VALUES
+os.chdir(savefile)
+name='WaferLight_AA_AS_SA_SS_MM.txt'
+fd=open(name,'w')
+rows=zip(waferlist,AA,AS,SA,SS,MM)
+for row in rows:
+    count=0
+    for i in row:
+        if count<(len(row)-1):
+            fd.write(str(i)+',')
+        else:
+            fd.write(str(i))
+        count+=1
+    fd.write('\n')
+fd.close()
---
+++ 3 Identify Hot Pixels.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 28 15:56:33 2018
+
+@author: Logan Rowe
+# -*- coding: utf-8 -*-
+
+Identify hot pixels from top pix list
+
+Top pix contains a list of the location of the top 20 pixels (by intensity) 
+for each 5x (microscopic) IR-GFP image.  This script will look at the top pixel
+in each image and if a pixel occurs as the top pixel more than 4 times it is marked
+as a hypersensitive (hot) pixel.  
+
+All pixels denoted as hypersensitive are then removed from the list of the top 20 pixels
+and the process is repeated until all hypersensitive pixels have been identified.
+
+Note: It is rare for a hypersensitive pixel to only occur 5-10 times.  If a pixel
+is hypersensitive it will occur many times, sometimes hundreds of times over the
+course of 3234 images.  
+
+Note: Hypersensitive pixels occur more frequently with long imaging sessions.  
+Turning the camera off and allowing it to cool to ambient temperature greatly
+mitigates the issue of hot pixels.  
+"""
+
+import numpy as np
+import os
+import glob
+
+savefile='C:\\Users\\Logan Rowe\\Desktop\\bowtie-defect-identification\\Light Levels\\Hot Pixel Lists'
+datafile='C:\\Users\\Logan Rowe\\Desktop\\bowtie-defect-identification\\Light Levels\\Data'
+waferdatafile='E:\\cSi Wafer Data\\DeltaVision Scripts\\Wafer Data Files'
+psdr=False #etched=True and as-cut wafers=False
+
+
+#BUILD WAFER LIST
+waferlist=[]
+os.chdir(waferdatafile)
+
+if psdr==True:
+    for i in glob.glob('*cSi*'):
+        waferlist.append(i.split('_')[1])
+else:
+    for i in glob.glob('*'):
+        if 'cSi' not in i:
+            waferlist.append(i.split('_')[1])
+
+#OR SPECIFY WHICH WAFER(S) YOU WANT TO RUN WITH THIS SCRIPT        
+waferlist=['50']
+            
+
+for wafer in waferlist:
+    print("Starting "+str(wafer))
+    os.chdir(datafile)
+    
+    data=wafer+'_TopPix.txt'
+    
+    hotpixels=[]
+    
+    data=np.genfromtxt(data,delimiter=',')
+    
+    dataset=list(set(data[:,0]))
+    
+    for i in dataset:
+        if data[:,0].tolist().count(i)>=4:
+            hotpixels.append(i)
+        
+    #CONVERT TO LIST TO OBSERVE FIRST PIXEL IN LIST AND REMOVE IF IT IS HOT
+    datalist=data.tolist()
+    while True:
+        count=0 #Track removed hot pixels
+        for i in datalist:
+            try:
+                while i[0] in hotpixels:
+                    del i[0]
+                    count+=1
+            except:
+                break
+        
+        print(count)
+                
+        #REBUILD ARRAY AND CHECK FOR NEW HOT PIXELS
+        pixlist=[i[0] for i in datalist if len(i)!=0]
+        for i in pixlist:
+            if pixlist.count(i)>=4:
+                hotpixels.append(i)
+          
+        if count==0:
+            break
+    
+    hotpixels=list(set(hotpixels))
+    hotpixels=[int(i) for i in hotpixels]
+    
+    print(hotpixels)
+    print(len(hotpixels))
+    
+    #SAVE HOT PIXEL LIST
+    os.chdir(savefile)
+    name=wafer+"_hotpix.txt"
+    fd=open(name,'w')
+    for i in hotpixels:
+        fd.write(str(i))
+        fd.write('\n')
+    fd.close()
+
