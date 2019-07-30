c8ba8bae73893801b5b5fb18df36b8f063f1ffd2
Logan Rowe <rowe1227@gmail.com>
2019-07-29 20:58:10 -0500

    Updated pipeline for cleaning and transforming data

--- 9_fit_transform_test.py
+++ 9_fit_transform_test.py
@@ -14,8 +14,15 @@ from pandas.plotting import scatter_matrix
 from sklearn.preprocessing import OneHotEncoder,StandardScaler
 from sklearn.svm import SVC
 from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.pipeline import Pipeline,FeatureUnion
+from sklearn.impute import SimpleImputer
+
 
 from scipy.stats import expon,reciprocal
+
+import data_prep as dp
+
+from imp import reload
 
 
 
@@ -27,19 +34,62 @@ from scipy.stats import expon,reciprocal
 data_dir='C:\\Users\\Logan Rowe\\Desktop\\bowtie-defect-identification\\Wafer_Images\\bowtie-training-data'
 X=np.load(data_dir+'\\thetaM_theta0_theta45_std0_std45_sh0_sh45_bow-bool_train_144_dim.npy')
 
+
+################################################
+# ADD COLUMN NAMES AND CONVERT TO PANDAS DF
+################################################
+def numpy_to_pd(array,column_names):
+    '''
+    array: numpy array
+    column_names: list of strings where each item is a column name
+    
+    Example:
+        array=np.array([[1,2],[3,4]])
+        column_names=['c_1','c_2']
+        X=numpy_to_pd(array,column_names)
+        print(X)
+        >>>pandas.DataFrame(dict({'c_1':[1,3],'c_2':[2,4]}))
+    '''
+    pd_dict=dict()
+    for (name,index) in zip(column_names,range(len(column_names))):
+        pd_dict[name]=array[:,index]
+    return pd.DataFrame(pd_dict)
+
 c1=['thetaM','theta0','theta45','std0','std45']
 c2=['sh0_{0}'.format(str(i)) for i in range(72)]
-c3=['sh0_{0}'.format(str(i)) for i in range(72)]
-c4=['bowtie']
-
+c3=['sh45_{0}'.format(str(i)) for i in range(72)]
+c4=['bowties']
 column_names=c1+c2+c3+c4
 
-X=pd.DataFrame(X)
+X=numpy_to_pd(X,column_names)
 
+################################################
+# SPLIT DATA INTO TEST AND TRAIN BOTH BALANCED
+# WITH RESPECT TO (NON)BOWTIES
+################################################
 split=StratifiedShuffleSplit(n_splits=1,test_size=0.2,random_state=42)
-for train_index, test_index in split.split(X,X[:,-1]):
-    train_set=X.loc[train_index]
-    test_set=X.loc[test_index]
+for train_index, test_index in split.split(X,X['bowties']):
+    train=X.loc[train_index]
+    test=X.loc[test_index]
+    
+print(test.shape)
+    
+X_=dp.reduce_features_in_sweep(first_index_of_sweep_in_X=list(X.columns).index('sh0_0')).transform(test)
+X_=dp.combine_theta_peaks().transform(X_)
+
+
+corr_matrix=X_.corr()
+#corr_vals=corr_matrix['bowties'].sort_values(ascending=False)
+#print(corr_vals)
+print(X_.shape)
+
+'''    
+pipeline=Pipeline([('Imputer',SimpleImputer(strategy='mean')),
+        ('Reduce Features',dp.reduce_features_in_sweep(reduced_circle_sweep_res=2, first_index_of_sweep_in_X=list(X.columns).index('sh0_0'))),
+                   ])
+'''
+
+
 
 #Attribute Adder
 ##Generate new attribute absolute difference of shear0 and shear 45 max points
--- data_prep.py
+++ data_prep.py
@@ -9,15 +9,16 @@ from sklearn.base import BaseEstimator,TransformerMixin
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 import numpy as np
+import pandas as pd
 
 class reduce_features_in_sweep(BaseEstimator,TransformerMixin):
-    def __init__(self,reduced_circle_sweep_res=1,first_index_of_sweep_in_X=5,bowtie_identifier=True):
+    def __init__(self,reduced_circle_sweep_res=2,first_index_of_sweep_in_X=5,bowtie_identifier=True):
         self.reduced_res=reduced_circle_sweep_res #Reduce the resolution of the circle sweep by a factor of 1,2,3,4,6,8, or 9
         self.first_index=first_index_of_sweep_in_X #X[:,?] is the index that the bowtie circle sweep starts on
         self.bowtie_identifier=bowtie_identifier #Is the identifier (bowtie: 1, nonbowtie: 0) included in X or already split from X
     def fit(self,X,y=None):
-        return X
-    def transform(self,X):
+        return self
+    def transform(self,X, y=None):
         
         #Reduce the resolution of the circle sweep by a factor of self.reduced_res
         mask=[bool((i)%self.reduced_res) for i in range(144)] #Masks every 3rd (or reduce value) data point in the shear 0 and shear 45 sweeps
@@ -27,21 +28,28 @@ class reduce_features_in_sweep(BaseEstimator,TransformerMixin):
         mask_thetas_stds=[True]*self.first_index
         mask=mask_thetas_stds+mask
     
-        X=X[:,mask]
-        return X
+        X_=X[X.columns[mask]]
+        return X_
 
+     
 class combine_theta_peaks(BaseEstimator,TransformerMixin):
-    def __init__(self,combine_and_remove=True):
+    def __init__(self,combine_and_remove=False):
         self.remove=combine_and_remove
     def fit(self,X,y=None):
-        return X
+        return self
     def transform(self,X):
-        theta_0=X[:,1]
-        theta_45=X[:,2]
-        theta_abs=np.abs(theta_0-theta_45)
+        theta_0=X['theta0'].dropna().tolist()
+        theta_45=X['theta45'].dropna().tolist()
+        theta_abs=[abs(i-j) for (i,j) in zip(theta_0,theta_45)]
+        theta_abs=pd.DataFrame(dict({'thetaDiff':theta_abs}))
         if self.remove:
-            X_=np.c_[X[:,0],theta_abs,X[:,3:]]
+            #X_=np.c_[X['thetaM'],X['std0':],theta_abs]
+            #X_=pd.join(X['thetaM'],theta_abs,X[X.columns[list(X.columns).index('sh0_0'):]])
+            X_=X['thetaM'].join(theta_abs)
+            X_=X_.join(X[X.columns[list(X.columns).index('sh0_0'):]])
             return X_
         else:
-            X_=np.c_[X[:,0:3],theta_abs,X[:,3:]]
+            #X_=pd.join(X['thetaM','theta0','theta45'],theta_abs,X[X.columns[list(X.columns).index('sh0_0'):]])
+            #X_=X[['thetaM','theta0','theta45']].join([theta_abs,X[list(X.columns)[list(X.columns).index('sh0_0'):]]])
+            X_=pd.concat([X[['thetaM','theta0','theta45']],theta_abs],sort=False)
             return X_
