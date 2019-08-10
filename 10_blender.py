288337c567c381c3a576e2d90f2bd223b9005cf7
Logan Rowe <rowe1227@gmail.com>
2019-08-10 02:11:54 -0500

    Upadted blender to have soft and hard options

--- 10_blender.py
+++ 10_blender.py
@@ -32,14 +32,28 @@ for file in glob.glob('*classifier.pkl'):
     globals()['%s'%'_'.join(file.split('_')[:2])]=joblib.load(file)
     
 # =============================================================================
-# DICTIONARY CONNECTING CLASSIFER TO TEST DATA
+# DICTIONARY CONNECTING {CLASSIFER:TEST DATA,}
 # =============================================================================
 clf_data={ET_img:ETC_img_test,
           ETC_circlesweep:ETC_circle_test,
           RF_circle:RFC_circle_test,
           RF_img:RFC_img_test,
           SVM_circlesweep:SVC_circle_test,
-          SVM_img:SVC_mp-img_test,
+          SVM_img:SVC_mpimg_test,
           XGBC_img:XGBC_img_test,
-          XGBRFC_img:XGB-RFC_img_test,
+          XGBRFC_img:XGBRFC_img_test,
         }
+
+# =============================================================================
+# SPLIT DATA AND MAKE PREDICTIONS WITH CLASSIFIER
+# =============================================================================
+def predict(clf,soft=True):
+    data=np.array(clf_data[clf])
+    X,y=data[:,:-1],data[:,-1]
+    if soft:
+        p=clf.predict_proba(X)
+    else:
+        p=clf.predict(X)
+    return(p,y)
+
+p_ET_img,y_ET_img=predict(ET_img)
