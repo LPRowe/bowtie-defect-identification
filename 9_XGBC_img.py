f55c17cdbf88245f6ffc89e87e80bcafb84014d8
Logan Rowe <rowe1227@gmail.com>
2019-08-09 22:37:25 -0500

    added final XGBC classifier and logloss images

--- 9_XGBC_img.py
+++ 9_XGBC_img.py
@@ -87,6 +87,7 @@ if seeking:
     
     X_train_trans=pipeline.fit_transform(X_train)
     X_val_trans=pipeline.fit_transform(X_val)
+    X_test_trans=pipeline.fit_transform(X_test)
     
     # =========================================================================
     # Convert back to panda dataframe because XGBoost and Scipy dont play nice
@@ -96,14 +97,15 @@ if seeking:
     
     X_train_trans=dp.numpy_to_pd(X_train_trans,column_names_xgb)
     X_val_trans=dp.numpy_to_pd(X_val_trans,column_names_xgb)
+    X_test_trans=dp.numpy_to_pd(X_test_trans,column_names_xgb)
 
     
     
-    param_grid={ 'gamma':[0], 
+    param_grid={ 'gamma':[0.05], 
                 'learning_rate':[0.1],
-                'max_depth':[3], 
+                'max_depth':[7], 
                 'min_child_weight':[1], 
-                'n_estimators':[100], 
+                'n_estimators':[50], 
                 'n_jobs':[-1], 
                 'objective':['binary:logistic'], 
                 'random_state':[42], 
@@ -144,26 +146,43 @@ if seeking:
 # =============================================================================
 params['n_estimators']=100
 
-#clf=xgboost.XGBRFClassifier(**params,n_estimators=100,n_jobs=-1,random_state=42)
 clf=xgboost.XGBClassifier(**params)
-eval_set=[(X_train_trans,y_train),(X_val_trans,y_val)]
-clf.fit(X_train_trans,y_train,eval_metric='error',eval_set=eval_set,verbose=10)
+eval_set=[(X_train_trans,y_train),(X_val_trans,y_val),(X_test_trans,y_test)]
+eval_metric=['error','logloss']
+clf.fit(X_train_trans,y_train,eval_metric=eval_metric,eval_set=eval_set,verbose=10)
 
 evals_result=clf.evals_result()
+
+#Errors
 train_errors=evals_result['validation_0']['error']
 val_errors=evals_result['validation_1']['error']
+test_errors=evals_result['validation_2']['error']
+
+#Logloss Errors
+train_errors_log=evals_result['validation_0']['logloss']
+val_errors_log=evals_result['validation_1']['logloss']
+test_errors_log=evals_result['validation_2']['logloss']
 
 N=np.linspace(1,params['n_estimators'],params['n_estimators'])
 
 plt.close('all')
-#Plot error
-plt.figure()
+#Plot error 
+plt.figure(1)
 plt.plot(N,val_errors,'b-')
 plt.plot(N,train_errors,'r-')
-plt.legend(['Validation','Training'])
+plt.plot(N,test_errors,'g-')
+plt.legend(['Validation','Training','Testing'])
 plt.xlabel('Number of Estimators')
 plt.ylabel('Error')
 
+#Plot logloss error
+plt.figure(2)
+plt.plot(N,val_errors_log,'b-')
+plt.plot(N,train_errors_log,'r-')
+plt.plot(N,test_errors_log,'g-')
+plt.legend(['Validation','Training','Testing'])
+plt.xlabel('Number of Estimators')
+plt.ylabel('Logloss Error')
 
 y_preds=clf.predict(X_test)
 
@@ -172,9 +191,46 @@ P,R,F=precision_score(y_test,y_preds),recall_score(y_test,y_preds),f1_score(y_te
 
 print(P,R,F,F_CV,params)
 
-
-final_params_selected=False
+# =============================================================================
+#  Based on the logloss curves the optimal number of estimators is
+#  between 46 and 58 so we will run it for 70 and use early stopping
+# =============================================================================
+params['n_estimators']=70
+final_params_selected=True
 if final_params_selected:
+    # =============================================================================
+    #  Combine training and validation sets to increase training data
+    # =============================================================================
+    X_train_full=pd.concat([X_train_trans,X_val_trans])
+    
+    clf=xgboost.XGBClassifier(**params)
+    eval_set=[(X_train_trans,y_train),(X_test_trans,y_test)]
+    eval_metric=['error','logloss']
+    clf.fit(X_train_trans,y_train,eval_metric=eval_metric,eval_set=eval_set,verbose=5,early_stopping_rounds=5)
+    
+    evals_result=clf.evals_result()
+    
+    #Logloss Errors
+    train_errors_log_2=evals_result['validation_0']['logloss']
+    test_errors_log_2=evals_result['validation_1']['logloss']
+    
+    N_2=np.linspace(1,len(test_errors_log_2),len(test_errors_log_2))
+    #Plot logloss error
+    plt.figure(3)
+    plt.plot(N_2,train_errors_log_2,'r-')
+    plt.plot(N_2,test_errors_log_2,'g-')
+    plt.legend(['Training','Testing'])
+    plt.xlabel('Number of Estimators')
+    plt.ylabel('Logloss Error')
+    
+    y_preds=clf.predict(X_test)
+    
+    F_CV=grid_search.best_score_    
+    P,R,F=precision_score(y_test,y_preds),recall_score(y_test,y_preds),f1_score(y_test,y_preds)
+    
+    print(P,R,F,F_CV,params)
+    
+    
     joblib.dump(clf,"C:\\Users\\Logan Rowe\\Desktop\\bowtie-defect-identification\\classifiers\\XGBC_img_classifier.pkl")
 
     
